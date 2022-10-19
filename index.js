11ac9e5e167007f4708d64325b10dbd747911843
Seiya830 <seiya@MacBook-Pro.local>
2022-10-19 16:55:41 +0900

    スライドショー完成

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ ham.addEventListener("click", function () {
   ham.classList.toggle("active");
   nav.classList.toggle("active");
 });
+
+//スライドショーで画像を入れ替える処理
+const img_src = ["./img/language-school-834138_1280.jpg", "./img/students-702094_1280.jpg", "./img/woman-4702060_1280.jpg"];
+
+let num = -1;
+
+function slide_time() {
+  if(num === 2) {
+    num = 0;
+  } else {
+    num ++;
+  }
+  //src属性をimg_srcに変更
+  document.getElementById("slide-img").src = img_src[num];
+}
+//1.3秒後に表示切り替え
+setInterval(slide_time, 1300);

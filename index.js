9eb52a69cad23f651477c20365c7f7e9946c52f3
Seiya830 <seiya@MacBook-Pro.local>
2022-11-14 10:41:21 +0900

    jsにコメント追加

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ハンバーガーメニュー
 const ham = document.querySelector("#js-hamburger");
 const nav = document.querySelector("#js-nav");
 
@@ -7,31 +8,36 @@ ham.addEventListener("click", function () {
 });
 
 // スライドショーで画像を入れ替える処理
-// const img_src = ["./img/language-school-834138_1280.jpg", "./img/students-702094_1280.jpg", "./img/woman-4702060_1280.jpg"];
+const img_src = ["./img/language-school-834138_1280.jpg", "./img/students-702094_1280.jpg", "./img/woman-4702060_1280.jpg"];
 
 let num = -1;
 
 function slide_time() {
+  // 最後の画像(インデックス2)が表示されたら
   if(num === 2) {
+  // 最初の画像を表示させる
     num = 0;
   } else {
+  // 0か1の場合はnumに1を足して次の画像のインデックスを指定
     num ++;
   }
-  //src属性をimg_srcに変更
+  // slide-imgを取得し、そのsrc属性をimg_srcに変更
   document.getElementById("slide-img").src = img_src[num];
 }
 //1.3秒後に表示切り替え
-// setInterval(slide_time, 1300);
+setInterval(slide_time, 1300);
 
+// フェードインアニメーション
 $(function() {
   $(".inview_re").on("inview", function (event, isInView) {
     if (isInView) {
+    // inview_reが表示されると、is-showが追加
       $(this).stop().addClass("is-show");
     }
   });
 });
 
-// 動きのきっかけとなるアニメーション名を定義
+// ズームインアニメーション
 function zoomAnime() {
   // ズームインのきっかけのクラス名と動きのクラス名の設定
   $('.zoomInTrigger').each(function() {
